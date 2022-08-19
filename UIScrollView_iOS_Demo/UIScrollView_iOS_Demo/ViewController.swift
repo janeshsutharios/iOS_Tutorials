f0a3187f174d3cd823293fe424961fa64f9e4ff1
Jack <janesh.stack@gmail.com>
2022-08-19 20:23:48 +0530

    added UISCrollView demo

---
+++ UIScrollView_iOS_Demo/UIScrollView_iOS_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScrollView_iOS_Demo
+//
+//  Created by Janesh Suthar on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

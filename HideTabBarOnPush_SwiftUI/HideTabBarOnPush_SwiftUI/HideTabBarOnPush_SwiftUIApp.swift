6fb330553784d926c60a3b3b05087388dbf475d9
Jack <janesh.stack@gmail.com>
2022-09-04 18:20:17 +0530

    Hide Tab bar on swiftUI code.

---
+++ HideTabBarOnPush_SwiftUI/HideTabBarOnPush_SwiftUI/HideTabBarOnPush_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideTabBarOnPush_SwiftUIApp.swift
+//  HideTabBarOnPush_SwiftUI
+//
+//  Created by JaneshSwift.com  on 04/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HideTabBarOnPush_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabBarView()
+        }
+    }
+}
---
+++ HideTabBarOnPush_SwiftUI/ReadMe.md
@@ -0,0 +1,3 @@
+Tutorial about iOS App Development For Tutorial www.JaneshSwift.com
+
+This Tutorial link -> https://janeshswift.com/ios/swiftui/how-to-hide-tabbar-on-push-with-swiftui/
